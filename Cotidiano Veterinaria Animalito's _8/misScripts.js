9a936e95b9603e31eaf76162c4e935d00b47c598
ArianCR09 <gtgubarian@gmail.com>
2025-05-30 16:11:36 -0600

    Seccion 10

--- Cotidiano Veterinaria Animalito's _8/misScripts.js
+++ Cotidiano Veterinaria Animalito's _8/misScripts.js
@@ -5,12 +5,13 @@ class animal {
                 this.edad = edad;
             }
             informacion() {
-                return `Nombre ${this.nombre} Peso ${this.peso} Edad ${this.edad}`;
+                return `Nombre: ${this.nombre}, Peso: ${this.peso}, Edad: ${this.edad}`;
             }
         }
+
         class Perro extends animal {
             constructor(nombre, peso, edad, Raza) {
-                Super(nombre, peso, edad);
+                super(nombre, peso, edad);
                 this._Raza = Raza;
             }
             get Raza() {
@@ -19,13 +20,14 @@ class animal {
             set Raza(nuevaRaza) {
                 this._Raza = nuevaRaza;
             }
-
+            informacion() {
+                return `${super.informacion()}, Raza: ${this._Raza}`;
+            }
         }
-        let perro1 = new Perro("Candy", "20kg", 10, "Pitbul");
 
         class Gato extends animal {
             constructor(nombre, peso, edad, sexo) {
-                Super(nombre, peso, edad);
+                super(nombre, peso, edad);
                 this._sexo = sexo;
             }
             get sexo() {
@@ -34,12 +36,14 @@ class animal {
             set sexo(nuevosexo) {
                 this._sexo = nuevosexo;
             }
+            informacion() {
+                return `${super.informacion()}, Sexo: ${this._sexo}`;
+            }
         }
-        let Gato1 = new Gato("Miausculos", "10kg", 5, "Masculino");
 
         class Conejo extends animal {
             constructor(nombre, peso, edad, color) {
-                Super(nombre, peso, edad);
+                super(nombre, peso, edad);
                 this._color = color;
             }
             get color() {
@@ -48,12 +52,24 @@ class animal {
             set color(nuevocolor) {
                 this._color = nuevocolor;
             }
+            informacion() {
+                return `${super.informacion()}, Color: ${this._color}`;
+            }           
         }
+
+        let perro1 = new Perro("Candy", "20kg", 10, "Pitbull");
+        let gato1 = new Gato("Miausculos", "10kg", 5, "Masculino");
         let conejo1 = new Conejo("Rabittt", "2kg", 1, "gris");
 
-        let array1 = [perro1, Gato1, conejo1];
-        
-        document.getElementById("listaAnimales")
-        function mostrarAnimales() {
+        let array1 = [perro1, gato1, conejo1];
 
+        function mostrarAnimales() {
+            const lista = document.getElementById("listaAnimales");
+            lista.innerHTML = ""; 
+
+            for (let h = 0; h < array1.length; h++) {
+                let li = document.createElement("li");
+                li.textContent = array1[h].informacion();
+                lista.appendChild(li);
+            }
         }
